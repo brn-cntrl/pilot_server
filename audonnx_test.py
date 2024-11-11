b912f06001613c3b75a8c81b81d2b1fb401dd583
Howie Li <howie0326@gmail.com>
2024-11-11 14:10:38 -0800

    fixed the audonnx test, now the prediction should be more accurate.

--- audonnx_test.py
+++ audonnx_test.py
@@ -6,42 +6,72 @@ import joblib
 import os
 import pandas as pd
 import wave
+import librosa
+
+# # download the model from the notebook
+# model_root = 'model'
+# cache_root = 'cache'
+
+
+# audeer.mkdir(cache_root)
+# def cache_path(file):
+#     return os.path.join(cache_root, file)
+
+
+# url = 'https://zenodo.org/record/6221127/files/w2v2-L-robust-12.6bc4a7fd-1.1.0.zip'
+# dst_path = cache_path('model.zip')
+
+# if not os.path.exists(dst_path):
+#     audeer.download_url(
+#         url, 
+#         dst_path, 
+#         verbose=True,
+#     )
+    
+# if not os.path.exists(model_root):
+#     audeer.extract_archive(
+#         dst_path, 
+#         model_root, 
+#         verbose=True,
+#     )
 
 CLF = joblib.load('classifier/emotion_classifier.joblib')
 RECORDING_FILE = 'tmp/recording.wav'
 MODEL_ROOT = "model"
 AUDONNX_MODEL = audonnx.load(MODEL_ROOT)
 
-def normalize_audio(audio): # Necessary for SER task
-    audio_array = audio / np.max(np.abs(audio))
-    return audio_array
+# I think these methods are not needed, if we use librosa package to load the model.
 
-def get_wav_as_np(filename):
-    """
-    Loads the entire wav file stored in tmp and returns it as a normalized numpy array.
-    Arguments: 
-        - the path and filename of the wav file
-    Returns: 
-        - the wav file as a numpy array
-    Exception: 
-        - if the file is not found
-    """
-    try:
-        with wave.open(filename, 'rb') as wf:
-            num_channels = wf.getnchannels()
-            num_frames = wf.getnframes()
-            signal = wf.readframes(num_frames)
-            signal = np.frombuffer(signal, dtype=np.int16).astype(np.float32)
-            signal = signal / np.iinfo(np.int16).max
+# def normalize_audio(audio): # Necessary for SER task
+#     audio_array = audio / np.max(np.abs(audio))
+#     return audio_array
 
-            if num_channels == 2:
-                signal = signal.reshape(-1, 2)
-                signal = signal.mean(axis=1)
+# def get_wav_as_np(filename):
+#     """
+#     Loads the entire wav file stored in tmp and returns it as a normalized numpy array.
+#     Arguments: 
+#         - the path and filename of the wav file
+#     Returns: 
+#         - the wav file as a numpy array
+#     Exception: 
+#         - if the file is not found
+#     """
+#     try:
+#         with wave.open(filename, 'rb') as wf:
+#             num_channels = wf.getnchannels()
+#             num_frames = wf.getnframes()
+#             signal = wf.readframes(num_frames)
+#             signal = np.frombuffer(signal, dtype=np.int16).astype(np.float32)
+#             signal = signal / np.iinfo(np.int16).max
 
-            return signal
-    except Exception as e:
-        print("Couldn't locate an audio file.")
-        return None
+#             if num_channels == 2:
+#                 signal = signal.reshape(-1, 2)
+#                 signal = signal.mean(axis=1)
+
+#             return signal
+#     except Exception as e:
+#         print("Couldn't locate an audio file.")
+#         return None
     
 def predict_emotion(audio_chunk):
     """
@@ -73,6 +103,8 @@ def predict_emotion(audio_chunk):
     if AUDONNX_MODEL is None:
         raise ValueError("The AUDONNX_MODEL is not initialized. Please load the model before calling this function.")
     
+    # Here we set the sampling_rate to 16000hz, just like what the notebook have set to, but our audio is 44100hz which
+    # is causing the issue of inaccuracy.
     sampling_rate = 16000
     hidden_states_feature = audinterface.Feature(
         AUDONNX_MODEL.labels('hidden_states'),
@@ -97,7 +129,8 @@ def predict_emotion(audio_chunk):
     return prediction[0]
 
 if __name__ == "__main__":  
-    signal = get_wav_as_np(RECORDING_FILE)
-    normed_sig = normalize_audio(signal)
-    prediction = predict_emotion(normed_sig)
+    signal, orig_sr = librosa.load(RECORDING_FILE, sr=None)
+    print(f"Original sampling rate: {orig_sr} Hz")
+    signal_resampled = librosa.resample(signal, orig_sr=orig_sr, target_sr=16000)
+    prediction = predict_emotion(signal_resampled)
     print(f"Predicted emotion: {prediction}")
