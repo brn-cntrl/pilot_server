806730d08e5fd0a406fb1e1388f2d69b77c6e562
Brian Cantrell <brncntrl@Brians-MBP.attlocal.net>
2025-01-19 16:43:45 -0800

    implemented csv logic in subject manager

--- subject_manager.py
+++ subject_manager.py
@@ -1,20 +1,5 @@
 from aws_handler import AWSHandler
-
-# This class will hold all the data for a single subject
-# The data will be stored in a dictionary with the following keys
-# ID: The ID of the subject
-# Date: The date the data was collected
-# Name: The name of the subject
-# Email: The email of the subject
-# Student_Data: A JSON object that holds the student survey data
-# Task_ID: A list of tuples that hold the timestamp for when a task is started (including idling between tasks) and the task id.
-# Test_Transcriptions: A list of JSON objects that hold the transcript, SER values, and timestamps for the test data
-# VR_Transcriptions_1: A list of JSON objects that hold the transcript, SER values, and timestamps for the VR data
-# VR_Transcriptions_2: A list of JSON objects that hold the transcript, SER values, and timestamps for the VR data
-# Biometric_Baseline: A list of JSON objects that hold the biometric baseline data
-# Biometric_Data: A list of JSON objects that hold the biometric data
-# NOTE: All data will be uploaded to the AWS database once the subject has completed the study. 
-# A CSV file will also be created for the subject
+from csv_handler import CSVHandler
 
 class SubjectManager:
     def __init__(self) -> None:
@@ -24,13 +9,10 @@ class SubjectManager:
             'Name': '',
             'Email': '',
             'student_data': {},
-            'Task_ID': [],
             'Test_Transcriptions': [], 
-            'VR_Transcriptions_1': [], 
-            'VR_Transcriptions_2': [], 
-            'Biometric_Baseline': [],
-            'Biometric_Data': [], 
-            'SER_Baseline': []
+            'VR_Transcriptions': [],  
+            'SER_Baseline': [],
+            'Event_Markers': []
         }
 
         # Create subject ID when the object is created
